2345787246a274b293cc83b6bf050ad370363df9
Seth Ockerman <sockerman@wisc.edu>
2026-04-23 22:44:53 -0500

    not relevant to benchmark

--- graphing/transfer.sh
+++
@@ -1,9 +0,0 @@
-rsync -av   --include='*/'   --include='*.csv'   --include='*.npy' --include='*.out' --include='submit.sh' --exclude='*'   aurora:/lus/flare/projects/AuroraGPT/sockerman/cleanEvalPaperResults/insert/ ./insert/
-rsync -av   --include='*/'   --include='*.csv'   --include='*.npy' --include='*.out' --include='submit.sh' --exclude='*'   polaris:/eagle/projects/argonne_tpc/sockerman/evalPaperResults/insert/ ./insert/
-
-rsync -av   --include='*/'   --include='*.csv'   --include='*.txt' --include='*.out' --include='submit.sh' --exclude='*'   aurora:/lus/flare/projects/AuroraGPT/sockerman/cleanEvalPaperResults/index/ ./aurora/
-rsync -av   --include='*/'   --include='*.csv'   --include='*.txt' --include='*.out' --include='submit.sh' --exclude='*'   polaris:/eagle/projects/argonne_tpc/sockerman/evalPaperResults/index/ ./index/
-
-
-rsync -av   --include='*/'   --include='*.csv'   --include='*.txt' --include='*.out' --include='submit.sh' --include='*.go' --exclude='*'   aurora:/lus/flare/projects/AuroraGPT/sockerman/cleanEvalPaperResults/query/ ./aurora/
-rsync -av   --include='*/'   --include='*.csv'   --include='*.txt' --include='*.out' --include='submit.sh' --exclude='*'   polaris:/eagle/projects/argonne_tpc/sockerman/evalPaperResults/index/ ./index/
